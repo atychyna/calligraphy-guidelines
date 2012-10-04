9e84c45b3cd5bab5bc3f79d1b7b81792e01e0ba2
Anton Tychyna <anton.tychyna@fredhopper.com>
2012-10-04 18:34:57 +0200

    - units of measure selector added (mm/inches) - KnockoutJS model implemented

--- src/main/scala/GuidelinesServlet.scala
+++ src/main/scala/GuidelinesServlet.scala
@@ -2,18 +2,32 @@ import java.awt.Color
 import org.scalatra.ScalatraServlet
 
 class GuidelinesServlet extends ScalatraServlet {
+    def param(name: String) = {
+        params.get(name).filter(_.trim.nonEmpty)
+    }
+
     post("/guidelines") {
         val nibSize = params("nibSize") match {
-            case "custom" => Some(params("customNibSize").toFloat)
-            case s: String => Some(s.toFloat)
+            case s: String if !s.isEmpty => Some(s.toFloat)
             case _ => None
+        }
+        val units = params("units") match {
+            case "inches" => Inches.apply _
+            case "mm" => Mm.apply _
+        }
+        val sizeInNibs = params.getOrElse("sizeInNibs", "off") match {
+            case "on" => true
+            case _ => false
         }
         contentType = "application/pdf"
         response.addHeader("Content-Disposition", "filename=guidelines-%.1f.pdf" format (nibSize.getOrElse(0f)))
+        val descender = param("descenderHeight").map(_.toFloat)
+        val ascender = param("ascenderHeight").map(_.toFloat)
+        val body = param("bodyHeight").map(_.toFloat)
         val lines: Array[Line] = Array(
-            Line(params.get("descenderHeight").map(_.toFloat * nibSize.get).getOrElse(nibSize.get * 3), 0.5f),
-            Line(params.get("ascenderHeight").map(_.toFloat * nibSize.get).getOrElse(nibSize.get * 3), color = Color.decode("#DCDCDC")),
-            Line(params.get("bodyHeight").map(_.toFloat * nibSize.get).getOrElse(nibSize.get * 5), color = Color.decode("#DCDCDC"))
+            Line(if (sizeInNibs) units(descender.getOrElse(3f) * nibSize.get) else units(descender.getOrElse(3 * nibSize.get)), 0.5f),
+            Line(if (sizeInNibs) units(ascender.getOrElse(3f) * nibSize.get) else units(ascender.getOrElse(3 * nibSize.get)), color = Color.decode("#DCDCDC")),
+            Line(if (sizeInNibs) units(body.getOrElse(5f) * nibSize.get) else units(body.getOrElse(5 * nibSize.get)), color = Color.decode("#DCDCDC"))
         )
         PDFGuidelines(GuidelinesConfig(nibSize), lines).save(response.getOutputStream)
     }
--- src/main/scala/PDFGuidelines.scala
+++ src/main/scala/PDFGuidelines.scala
@@ -3,7 +3,25 @@ import java.io.OutputStream
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream
 import org.apache.pdfbox.pdmodel.{PDPage, PDDocument}
 
-case class Line(distance: Float, width: Float = 1f, color: Color = Color.decode("#ECECEC"))
+sealed abstract class Unit {
+    val userSpaceUnitDPI = 72
+
+    def units: Float
+}
+
+case class Mm(mm: Float) extends Unit {
+    val units = mmToUnits(mm)
+
+    private def mmToUnits(mm: Float) = 1 / (10 * 2.54f) * userSpaceUnitDPI
+}
+
+case class Inches(inches: Float) extends Unit {
+    val units = inchesToUnits(inches)
+
+    private def inchesToUnits(inches: Float) = inches * userSpaceUnitDPI
+}
+
+case class Line(distance: Unit, width: Float = 1f, color: Color = Color.decode("#ECECEC"))
 
 class PDFGuidelines(val config: GuidelinesConfig, val lines: Array[Line]) extends Guidelines(config) {
     require(config != null, "GuidelinesConfig should be provided")
@@ -20,7 +38,7 @@ class PDFGuidelines(val config: GuidelinesConfig, val lines: Array[Line]) extend
         for {i <- 0 to segmentsCount
              l <- 0 until lines.length} {
             val line = lines(l)
-            distance -= inchesToUnits(line.distance)
+            distance -= line.distance.units
             stream.setLineWidth(line.width)
             stream.setStrokingColor(line.color)
             stream.drawLine(0, distance, box.getWidth, distance)
@@ -30,22 +48,17 @@ class PDFGuidelines(val config: GuidelinesConfig, val lines: Array[Line]) extend
         pdf.close()
     }
 
-    val userSpaceUnitDPI = 72
 
-    private def inchesToUnits(inches: Float) = inches * userSpaceUnitDPI
-
-    private def mmToUnits(mm: Float) = 1 / (10 * 2.54f) * userSpaceUnitDPI
-
-    private def segmentHeight = inchesToUnits((0f /: lines)(_ + _.distance))
+    private def segmentHeight = (0f /: lines)(_ + _.distance.units)
 }
 
 object PDFGuidelines {
 
     def apply(config: GuidelinesConfig) = {
         val lines: Array[Line] = Array(
-            Line(config.descender.orElse(config.nib.map(_ * 3)).get, 0.5f),
-            Line(config.descender.orElse(config.nib.map(_ * 3)).get, color = Color.decode("#DCDCDC")),
-            Line(config.descender.orElse(config.nib.map(_ * 5)).get, color = Color.decode("#DCDCDC"))
+            Line(Inches(config.descender.orElse(config.nib.map(_ * 3)).get), 0.5f),
+            Line(Inches(config.descender.orElse(config.nib.map(_ * 3)).get), color = Color.decode("#DCDCDC")),
+            Line(Inches(config.descender.orElse(config.nib.map(_ * 5)).get), color = Color.decode("#DCDCDC"))
         )
         new PDFGuidelines(config, lines)
     }
